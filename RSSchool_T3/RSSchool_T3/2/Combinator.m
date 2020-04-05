a1b94de108e447073db1a00e3c1d90b13c1b430a
TuTejsy <1111kaktus1111@gmail.com>
2020-04-05 13:21:26 +0300

    add solution to task 2

--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -2,6 +2,36 @@
 
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    int m = [[array firstObject] intValue];
+    int n = [[array lastObject] intValue];
+    
+    int x = 1;
+    NSUInteger nFactorial = [Combinator getFactorial:n];
+    
+    while (x < n) {
+        NSUInteger result = nFactorial / ([Combinator getFactorial:x] * [Combinator getFactorial:(n - x)]);
+        
+        if (result >= m) {
+            return @(x);
+        }
+        
+        x++;
+    }
+    
+    return nil;
 }
+
++ (NSUInteger)getFactorial:(int)num {
+    if(num < 2) {
+        return(1);
+    }
+
+    NSUInteger result = 1;
+    for (int i = num; i > 1; i--) {
+        result *= i;
+    }
+
+    return result;
+}
+
 @end
